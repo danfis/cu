e99b7732766dd62d00512c4d6298192af3046f6a
Daniel Fiser <danfis@danfis.cz>
2008-02-07 21:43:46 +0100

    Added notes about license to all files.

--- cu.c
+++ cu.c
@@ -1,3 +1,25 @@
+/***
+ * CU - C unit testing framework
+ * ---------------------------------
+ * Copyright (c)2007 Daniel Fiser <danfis@danfis.cz>
+ *
+ *
+ *  This file is part of CU.
+ *
+ *  CU is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation; either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  CU is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
