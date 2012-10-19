8434c02a95d0dedb85668c175944352c42fd1f2d
Daniel Fiser <danfis@danfis.cz>
2012-10-19 12:29:45 +0200

    Fixed make-release.sh script.

--- make-release.sh
+++ make-release.sh
@@ -7,7 +7,8 @@ COMMIT="$1"
 PREFIX=cu-$COMMIT
 FN=cu-$COMMIT.tar.gz
 CMD="git archive"
-FILES="COPYING COPYING.LESSER cu.c cu.h Makefile check-regressions"
+FILES="BSD-LICENSE CHANGELOG cu.c cu.h Makefile cu-check-regressions"
+FILES="$FILES debian/compat debian/control debian/make-changelog.py debian/rules"
 
 if [ "$COMMIT" = "" ]; then
     echo "Error: you must specify commit which should be packed"
